73d7738a354c69b8306becc1540fb92300bbe9c6
Sarness Prasad <sunny776@yahoo.com>
2021-05-13 16:46:40 -0700

    uploading files

---
+++ employee_directory/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <nav className="navbar">
+            <h1>Employee Directory</h1>
+            <p></p>
+        </nav>
+
+
+
+      );
+}
+ 
+export default Header;
---
+++ employee_directory/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import "../styles/stylesheet.css"
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            
+        </nav>
+
+
+
+      );
+}
+ 
+export default Navbar;
